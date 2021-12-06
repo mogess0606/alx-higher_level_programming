7998a141f04a7facafcd0bb3ed2787c51e31dc50
root <mogessmekonnen13@gmail.com>
2021-12-06 09:08:13 +0000

    0x05 python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    """Print x elements of a list'
+    """Print x elements of a list.
 
     Args:
        my_list (list): The list to print elements from.
