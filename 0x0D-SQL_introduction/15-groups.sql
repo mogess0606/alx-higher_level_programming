db52e04747b2c4fdf6e0aed770f628151ce6c9a4
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-01 05:00:27 -0800

    0x0D SQL Introduction

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Syntax that lists the number of records with the same score
 
-SELECT score,COUNT(score) FROM second_table  GROUP BY score ORDER BY score DESC;
+SELECT score,COUNT(score) AS number FROM second_table  GROUP BY score ORDER BY score DESC;
 
