a8c491f3cd674b867159f56b04e2aff612d5e357
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-01 04:05:32 -0800

    0x0D SQL Introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,10 +7,4 @@ score INT);
 
 --Syntax  to add multiples rows to second table
 
-insert into second_table(id,name,score) VALUES
-(1,'John',10),
-(2,'Alex',3),
-(3,'Bob',14),
-(4,'George',8);
-
-
+insert into second_table(id,name,score) VALUES(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
