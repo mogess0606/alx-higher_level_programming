87d7cecd2182e4baa934f2f36067731cc9217d70
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-02 05:50:34 -0800

    0x0E SQL more queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Syntax used to create database and table state on mysql
 
 CREATE DATABSE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT, name VARCHAR(256));
+USE  hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
