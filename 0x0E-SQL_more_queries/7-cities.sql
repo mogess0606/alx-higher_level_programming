a8a16661fe8ba98eb4456f69129f2961f508b04b
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-02 06:05:25 -0800

    0x0E SQL more queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Syntax used to create cities table
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE PRIMARY KEY AUTO_INCREMENT, state_id INT  FOREIGN KEY REFERENCES states(id), name varchar(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, name varchar(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+
 
