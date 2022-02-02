9e04418026b726327c2aca8a930bdaa9b9469725
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-02 04:20:33 -0800

    0x0E SQL more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Sytntax used to lis all privileges of mysql
 
-cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
