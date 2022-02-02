50d6030f603e048c235270e73015891c583853f3
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-02 06:30:32 -0800

    0x0E SQL more queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,8 @@
 --Syntax used to lists all shows contained in the database
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC; 
+    FROM tv_shows
+    INNER JOIN tv_show_genres
+    ON tv_show_genres.show_id=tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
+ASC;
