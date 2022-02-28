f73c44666f85b662c7c824e06aa5daa2d5c3c74d
mogess0606 <mogessmekonnen13@gmail.com>
2022-02-28 06:09:53 -0800

    node js warm up

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-export.addMeMaybe = function(num, theFunction) {
-	num++;
-	theFunction(num);
+export.addMeMaybe = function(number, theFunction) {
+	number++;
+	theFunction(number);
 };
 
