16a5342c1715e2ed8e9dc1d6269a1a6388ae6839
MogessMekonnen13@gmail.com <MogessMekonnen13@gmail.com>
2022-03-18 11:59:58 +0000

    0x10 python networking 0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that makes a message containing You got me!
-curl -sLX PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: School"
+curl -sLX PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: HolbertonSchool"
