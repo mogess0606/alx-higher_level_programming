34f558878bf8477065d7b9982780331f73d6183a
MogessMekonnen13@gmail.com <MogessMekonnen13@gmail.com>
2022-03-18 12:21:18 +0000

    0x11 python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetch web response
-"""
 
-import urllib.request
+#Python script that fetches https://alx-intranet.hbtn.io/status
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(str(content)[2:-1]))
+if __name__ == '__main__':
+    import urllib.request
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(html.__class__))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('ascii')))
